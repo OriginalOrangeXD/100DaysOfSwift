be93f101c632a1f37885d8c2b33001c21316987c
Robert De Angelis <deangelis.robert13@gmail.com>
2026-01-02 23:51:24 -0500

    conv

---
+++ conv/conv/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  conv
+//
+//  Created by Robby on 2026-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ conv/conv/convApp.swift
@@ -0,0 +1,17 @@
+//
+//  convApp.swift
+//  conv
+//
+//  Created by Robby on 2026-01-02.
+//
+
+import SwiftUI
+
+@main
+struct convApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
